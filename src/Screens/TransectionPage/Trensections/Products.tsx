22c74d7c7ce2310a46d8affc0f8fdabf412a925c
Oguzhan <oguzhanfatihkucuk@gmail.com>
2024-05-08 02:50:03 +0300

    Report Screen has been created.

--- src/Screens/TransectionPage/Trensections/Products.tsx
+++ src/Screens/TransectionPage/Trensections/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { View, FlatList, Text, SafeAreaView, ScrollView } from "react-native";
 import products from "../../../../Assets/product";  // Import your product data
 import ProductCard from "../../../../src/Components/productcard.tsx";
@@ -12,63 +12,57 @@ const ProductsScreen = () => {
   // @ts-ignore
   return (
     <SafeAreaView>
-     <ScrollView>
-      <View>
-        <View >
-          <Text>
-            Favorites
-          </Text>
-          <FlatList
-            horizontal={true}  // Enable horizontal scrolling
-            data={products}
-            renderItem={({ item }) => (
-              <View style={{ flexDirection: "row" }}>
-                <ProductCard product={item} />
-              </View>
-            )}
-            keyExtractor={(item) => item.id.toString()}
-          />
+      <ScrollView>
+        <TextInput
+          onChangeText={(e) => setSearchTerm(e)}
+          placeholder="Search products..."
+          style={{ backgroundColor: "transparent" }}
+          value={searchTerm}
+        />
+        <View>
+          {searchTerm ? (<View>
+            <Text>
+              New Products
+            </Text>
+            <FlatList
+              horizontal={true}  // Enable horizontal scrolling
+              data={filteredProducts2}
+              renderItem={({ item }) => (
+                <View style={{ flexDirection: "row" }}>
+                  <ProductCard product={item} />
+                </View>
+              )}
+              keyExtractor={(item) => item.id.toString()}
+            />
+          </View>) : (<><View>
+            <Text>
+              Favorites
+            </Text>
+            <FlatList
+              horizontal={true} // Enable horizontal scrolling
+              data={products}
+              renderItem={({ item }) => (
+                <View style={{ flexDirection: "row" }}>
+                  <ProductCard product={item} />
+                </View>
+              )}
+              keyExtractor={(item) => item.id.toString()} />
+          </View><Divider style={{ height: 15, backgroundColor: "transparent" }}></Divider><View>
+            <Text>
+              Hot Deals
+            </Text>
+            <FlatList
+              horizontal={true} // Enable horizontal scrolling
+              data={products}
+              renderItem={({ item }) => (
+                <View style={{ flexDirection: "row" }}>
+                  <ProductCard product={item} />
+                </View>
+              )}
+              keyExtractor={(item) => item.id.toString()} />
+          </View></>)}
         </View>
-        <Divider style={{ height: 15, backgroundColor: "transparent" }}></Divider>
-        <View >
-          <Text>
-            Hot Deals
-          </Text>
-          <FlatList
-            horizontal={true}  // Enable horizontal scrolling
-            data={products}
-            renderItem={({ item }) => (
-              <View style={{ flexDirection: "row" }}>
-                <ProductCard product={item} />
-              </View>
-            )}
-            keyExtractor={(item) => item.id.toString()}
-          />
-        </View>
-        <Divider style={{ height: 15, backgroundColor: "transparent" }}></Divider>
-        <View >
-          <TextInput
-            value={searchTerm}
-            onChangeText={(e) => setSearchTerm(e)}
-            placeholder="Search products..."
-          />
-          <Text>
-            New Products
-          </Text>
-          <FlatList
-            horizontal={true}  // Enable horizontal scrolling
-            data={filteredProducts2}
-            renderItem={({ item }) => (
-              <View style={{ flexDirection: "row" }}>
-                <ProductCard product={item} />
-              </View>
-            )}
-            keyExtractor={(item) => item.id.toString()}
-          />
-        </View>
-        <Divider style={{ height: 15, backgroundColor: "transparent" }}></Divider>
-      </View>
-     </ScrollView>
+      </ScrollView>
     </SafeAreaView>
   );
 };
