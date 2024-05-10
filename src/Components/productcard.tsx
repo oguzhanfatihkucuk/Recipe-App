39d94d09adf03a58e7c43694ab36c5ce86198d47
Oguzhan <oguzhanfatihkucuk@gmail.com>
2024-05-10 15:31:24 +0300

    Image source has been updated

--- src/Components/productcard.tsx
+++ src/Components/productcard.tsx
@@ -7,7 +7,7 @@ const ProductCard = ({ product }) => {
   return (
     <TouchableOpacity style={{height:160,width:130,margin:3}} >
       <View style={{ flex: 5, padding: 10, borderWidth: 1, borderRadius: 5, margin: 5 }}>
-        <Image source={{ uri: product.imageUrl || 'placeholder.png' }} style={{ width: 50, height: 50 }} />
+        <Image source={{ uri: product.imageUrl   }} style={{ width: 50, height: 50 }} />
         <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{product.title}</Text>
         <Text>Price: ${product.price || 'N/A'}</Text>
         <Text numberOfLines={2}>{product.description}</Text>
