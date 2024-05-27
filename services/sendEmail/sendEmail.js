d555a92410e4b89215039db830e73f5f4611df69
Oğuzhan Fatih Küçük <83452551+phoenix9897@users.noreply.github.com>
2024-05-28 02:20:04 +0300

    email services modularizated

---
+++ services/sendEmail/sendEmail.js
@@ -0,0 +1,30 @@
+// sendEmail.js
+import axios from 'axios';
+import Toast from 'react-native-toast-message';
+import MY_IP from "../../assets/js/myIp"; // Kullanılan toast kütüphanesini içe aktarın
+
+
+
+export const sendEmail = (email, myContent) => {
+  axios.post(`http://${MY_IP}:3002/send-email`, {
+    myAddress: email,
+    content: myContent
+  })
+    .then(response => {
+      Toast.show({
+        type: 'success',
+        text1: 'Success!!',
+        text2: 'Email sent successfully',
+        position: 'bottom'
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      Toast.show({
+        type: 'error',
+        text1: 'Error!!',
+        text2: 'Failed to send email',
+        position: 'bottom'
+      });
+    });
+};
