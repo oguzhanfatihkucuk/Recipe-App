9954bc0ac0dff1b682a6c46e156f3c53b1a1753c
Oğuzhan Fatih Küçük <83452551+phoenix9897@users.noreply.github.com>
2024-05-28 15:30:30 +0300

    PDF creating functionalty added.

--- src/Screens/ReportsScreen/Reports/Z-Report.tsx
+++ src/Screens/ReportsScreen/Reports/Z-Report.tsx
@@ -1,12 +1,48 @@
 import React, {} from "react";
-import { Alert, Button, FlatList, SafeAreaView, Text, View } from "react-native";
+import { Alert, Button, FlatList, Platform, SafeAreaView, Text, View } from "react-native";
 import { reports, reportsOffline } from "../../../../assets/js/reports";
-
+import RNHTMLtoPDF from 'react-native-html-to-pdf';
 const ZReports = () => {
+
+
+  //@ts-ignore
+  const createPDF = async (index,item) => {
+
+    try {
+      let PDFOptions = {
+        html: `<h2>${item}</h2>`,
+        fileName: "report"+index.toString(),
+        directory: Platform.OS === 'android' ? 'Downloads' : 'Documents',
+      };
+      let file = await RNHTMLtoPDF.convert(PDFOptions);
+      if (!file.filePath) return;
+      Alert.alert("PDF file saved your local devices Successfully","Your file directory is "+file.filePath);
+
+    } catch (error) {
+      // @ts-ignore
+      console.log('Failed to generate pdf', error.message);
+    }
+  };
+
   //@ts-ignore
   const renderItem = ({ item, index }) => {
     const onPressButton = () => {
-      Alert.alert('Rapor', `Rapor ${index + 1}: ${item}`);
+      Alert.alert(
+        'Rapor',
+        `Rapor ${index + 1}: ${item}`,
+        [
+          {
+            text: 'Yazdır',
+            //@ts-ignore
+            onPress: () => createPDF(index,item),
+          },
+          {
+            text: 'Kapat',
+          },
+        ],
+        { cancelable: false },
+      );
+
     };
 
     return (
@@ -21,7 +57,7 @@ const ZReports = () => {
     <SafeAreaView>
       <View style={{flexDirection:"row"}}>
         <Text>
-          Printed Recipes
+          Sended recipts.
         </Text>
         <FlatList
           data={reports}
@@ -30,7 +66,7 @@ const ZReports = () => {
           keyExtractor={(item, index) => index.toString()}
         />
         <Text>
-          Haven't Printed Recipes
+          Haven't Sended recipts.
         </Text>
         <FlatList
           data={reportsOffline}
